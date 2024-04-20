2618cee0bb0366ea1b78c847c0bba9319c22265a
Alexander <dayboval_official@mail.ru>
2024-04-20 14:36:13 +0500

    [change] Небольшие исправления файловой структуры и разметки

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,13 +1,13 @@
 import "../css/Entry.css";
 import React from "react";
-import fullLogo from "../img/full-logo.svg"
+import fullLogo from "/Users/daybo/Desktop/knowledge_base/markup/img/full-logo.svg"
 
 function Login() {
   return (
   <div className="entry">
-    <img className="entry__logo" src="./img/full-logo.svg" alt="Логотип базы знаний 'IKnow'"/>
+    <img className="entry__logo" src={fullLogo} alt="Логотип базы знаний 'IKnow'"/>
 
-    <form className="entry__login-form" action={fullLogo} method="post">
+    <form className="entry__login-form" action="http://127.0.0.1:8000/login/" method="post">
       <input className="entry__login-input" type="text" id="username" name="username" placeholder="Введите email" autoComplete="off"/>
       <input className="entry__login-input" type="password" id="password" name="password" placeholder="Введите пароль"/>
       <button className="entry__login-btn" type="submit">Войти</button>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Entry from "./components/Entry";
+import Main from "./components/Main";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Entry/>
+    <Main/>
   </React.StrictMode>
 );
