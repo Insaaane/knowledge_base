b3686c7600ad918978cb487e7ba8058e224b331b
Alexander <dayboval_official@mail.ru>
2024-04-30 19:38:20 +0500

    [change] Исправления вёрстки

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -4,14 +4,16 @@ import fullLogo from "/Users/daybo/Desktop/knowledge_base/markup/img/full-logo.s
 
 export default function Login() {
   return (
-  <div className="entry">
-    <img className="entry__logo" src={fullLogo} alt="Логотип базы знаний 'IKnow'"/>
+  <div className="entry__wrapper">
+    <div className="entry">
+      <img className="entry__logo" src={fullLogo} alt="Логотип базы знаний 'IKnow'"/>
 
-    <form className="entry__login-form" action="http://127.0.0.1:8000/login/" method="post">
-      <input className="entry__login-input" type="text" id="username" name="username" placeholder="Введите email" autoComplete="off"/>
-      <input className="entry__login-input" type="password" id="password" name="password" placeholder="Введите пароль"/>
-      <button className="entry__login-btn" type="submit">Войти</button>
-    </form>
+      <form className="entry__login-form" action="http://127.0.0.1:8000/login/" method="post">
+        <input className="entry__login-input" type="text" id="username" name="username" placeholder="Введите email" autoComplete="off"/>
+        <input className="entry__login-input" type="password" id="password" name="password" placeholder="Введите пароль"/>
+        <button className="entry__login-btn" type="submit">Войти</button>
+      </form>
+    </div>
   </div>
   );
 }
